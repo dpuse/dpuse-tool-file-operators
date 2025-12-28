7ac78789e35b57a1660fd9b155247cdb3d8cfb7e
Jonathan Terrell <terrell.jm@gmail.com>
2025-12-28 22:05:03 +0100

    v0.0.3

--- README.md
+++ README.md
@@ -6,8 +6,10 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](./LICENSE)
 
 <!-- DEPENDENCY_LICENSES_START -->
-
-| Name | Type | Installed | Latest | Latest Released | Deps | Document |
-| :--- | :--- | :-------: | :----: | :-------------- | ---: | :------- |
+|Name|Type|Installed|Latest|Latest Released|Deps|Document|
+|:-|:-|:-:|:-:|:-|-:|:-|
+|@datapos/datapos-shared|MIT|0.3.428|0.3.428|this month: 2025-12-28|0|[LICENSE](https://raw.githubusercontent.com/data-positioning/datapos-shared/main/LICENSE)|
+|chardet|MIT|2.1.1|2.1.1|1 month ago: 2025-10-29|0|[LICENSE](https://raw.githubusercontent.com/runk/node-chardet/master/LICENSE)|
+|file-type|MIT|21.2.0|21.2.0|this month: 2025-12-25|4|[license](https://raw.githubusercontent.com/sindresorhus/file-type/main/license)|
 
 <!-- DEPENDENCY_LICENSES_END -->
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,31 @@
  * File operations tool.
  */
 
-/** Vendor dependencies. */
+// Vendor dependencies.
+import chardet from 'chardet';
+import { fileTypeFromBuffer } from 'file-type';
 
-/** Framework dependencies. */
+// Framework dependencies.
 
-/** Tool. */
+// Tool.
 class Tool {
-    }
+    /**
+     * Preview remote file.
+     */
+    async previewRemoteFile(url: string, signal: AbortSignal, chunkSize?: number): Promise<DataViewPreviewConfig> {
+        const response = await fetch(encodeURI(url), { headers: { Range: `bytes=0-${chunkSize ?? DEFAULT_PREVIEW_CHUNK_SIZE - 1}` }, signal });
+        if (!response.ok) {
+            throw await buildFetchError(response, `Failed to fetch '${url}' file.`, 'datapos-connector-file-store-emulator|Connector|preview');
+        }
 
-    /** Exports */
+        const unit8Array = new Uint8Array(await response.arrayBuffer());
+        const fileType = await fileTypeFromBuffer(unit8Array);
+        if (fileType != undefined) throw new Error(`Files of type '${fileType.ext}' (mine '${fileType.mime}') not supported.`);
+
+        const dataViewPreviewConfig = previewUnit8Array(unit8Array);
+        return dataViewPreviewConfig;
+    }
+}
+
+// Exports.
 export { Tool };
