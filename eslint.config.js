2372194facac9d6c70d3f97bf24cf708207f811b
Jonathan Terrell <terrell.jm@gmail.com>
2026-07-04 12:50:36 +0200

    v0.0.42

--- eslint.config.js
+++ eslint.config.js
@@ -5,12 +5,7 @@ import { dpuseESLintConfig } from '@dpuse/eslint-config-dpuse';
 
 /** @type {import('eslint').Linter.Config[]} */
 const config = dpuseESLintConfig({
-    files: ['eslint.config.js', 'src/**/*.ts', 'vite.config.ts', 'vitest.config.ts'],
-    ignores: ['rust/**'],
-    importCoreModules: ['cloudflare:workers'],
-    tsconfigPath: './tsconfig.json',
-    tsconfigRootDir: import.meta.dirname,
-    rules: {}
+    ignores: ['rust/**']
 });
 
 export default config;
